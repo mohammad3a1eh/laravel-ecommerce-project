b53e227cca297cfba2582e81a3f1b2d5b15344f9
mohammad3a1eh <kinitegp@gmail.com>
2025-12-06 10:58:32 +0330

    Add update and delete routes for brands
    Introduces PUT and DELETE routes for updating and deleting brands, handled by BrandController. This enables full CRUD operations for the brands resource.
    
    Co-Authored-By: ehsan malek <151904176+ehsan-malek@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::prefix('dashboard')->name('admin.')->middleware(['auth', 'verified'])->gr
     Route::get('/brands/create', [BrandController::class, 'create'] )->name('brands.create');
     Route::post('/brands', [BrandController::class, 'store'])->name('brands.store');
     Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'] )->name('brands.edit');
+    Route::put('/brands/{brand}', [BrandController::class, 'update'])->name('brands.update');
+    Route::delete('/brands/{brand}', [BrandController::class, 'destroy'])->name("brands.destroy");
 });
 
 Route::get('/', function () {
